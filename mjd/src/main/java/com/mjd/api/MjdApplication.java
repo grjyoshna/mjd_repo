2bb4c249a85054dcb645c0b383c661d4fece49bf
grjyo <grjyo@LAPTOP-RG167QRB.attlocal.net>
2020-01-05 18:58:06 -0500

    first commit

---
+++ mjd/src/main/java/com/mjd/api/MjdApplication.java
@@ -0,0 +1,13 @@
+package com.mjd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjdApplication.class, args);
+	}
+
+}
---
+++ mjd/src/test/java/com/mjd/api/MjdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
